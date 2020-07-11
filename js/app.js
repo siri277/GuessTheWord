a0d4d34a5d665fa7c97d44253535fdd34d49d69f
siri <dsirichandana1703@gmail.com>
2020-07-11 13:50:04 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,95 @@
+const msg = document.querySelector(".msg");
+const guess = document.querySelector("input");
+const button1 = document.querySelector(".button1");
+const score = document.getElementById("score");
+
+let play = false;
+let newWords1 = "";
+let randWords1 = "";
+var timeleft = 60;
+var timerCount = 0;
+var count = 0;
+
+let sWords1 = ["tiger", "fox", "lion", "snake", "giraffe", "dog", "elephant", "horse", "zebra", "bear", "wolf",
+    "cat", "deer", "monkey", "sheep", "buffalo", "hippopotamus", "cheetah", "rhinoceros"];
+
+function start() {
+    window.location.href = "../static pages/index1.html";
+}
+function birds() {
+    window.location.href = "../static pages/index3.html";
+}
+function animals() {
+    window.location.href = "../static pages/index2.html";
+}
+function playagain() {
+    window.location.href = "../static pages/index1.html";
+}
+
+const createNewWords1 = () => {
+    let ranNum1 = Math.floor(Math.random() * sWords1.length);
+    //console.log(ranNum);
+    let newtempsWords1 = sWords1[ranNum1];
+    //console.log(newtempsWords.split(""));
+    return newtempsWords1;
+}
+const scrambleWords1 = (arr) => {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        let temp = arr[i];
+        //console.log(temp);
+        let j = Math.floor(Math.random() * (i + 1));
+        //console.log(i);
+        //console.log(j);
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    return arr;
+}
+function timer() {
+    var downtimer = setInterval(function () {
+        if (timeleft < 0) {
+            clearInterval(downtimer);
+            window.location.href = "../static pages/index4.html";
+        }
+        else {
+            document.getElementById("timer").innerText = "Time Left : " + timeleft;
+            timeleft -= 1;
+        }
+
+    }, 1000)
+}
+button1.addEventListener("click", function () {
+    timerCount += 1;
+
+    if (timerCount == 1) {
+        timer();
+    }
+    if (!play) {
+        play = true;
+        button1.innerHTML = "Guess";
+        guess.classList.toggle("hidden");
+        newWords1 = createNewWords1();
+        randWords1 = scrambleWords1(newWords1.split(""));
+        //console.log(randWords.join(""));
+        msg.innerHTML = randWords1;
+    }
+    else {
+        let tempword = guess.value;
+        if (tempword == newWords1) {
+            //console.log("Correct");
+            play = false;
+            msg.innerHTML = `AWESOME`;
+            count++;
+            score.innerText = "   Score : " + count;
+            button1.innerHTML = "NEXT";
+            guess.classList.toggle("hidden");
+            guess.value = "";
+        }
+        else {
+            //console.log("Incorrect");
+            msg.innerHTML = `OH NO!. Try Again: ${randWords1}`;
+        }
+    }
+})
+
+
---
+++ js/app1.js
@@ -0,0 +1,88 @@
+const msg = document.querySelector(".msg");
+const guess = document.querySelector("input");
+const button2 = document.querySelector(".button2");
+const score = document.getElementById("score");
+
+let play = false;
+let newWords2 = "";
+let randWords2 = "";
+var timeleft = 60;
+var timerCount = 0;
+var count = 0;
+
+let sWords2 = ["pigeon", "crow", "parrot", "owl", "swan", "peacock", "penguin", "ostrich", "vulture", "eagle", "hen", "sparrow",
+    "dove", "flamingo", "duck", "goose", "cuckoo", "kingfisher", "falcon"];
+
+
+const createNewWords2 = () => {
+    let ranNum2 = Math.floor(Math.random() * sWords2.length);
+    //console.log(ranNum);
+    let newtempsWords2 = sWords2[ranNum2];
+    //console.log(newtempsWords.split(""));
+    return newtempsWords2;
+}
+const scrambleWords2 = (arr) => {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        let temp = arr[i];
+        //console.log(temp);
+        let j = Math.floor(Math.random() * (i + 1));
+        //console.log(i);
+        //console.log(j);
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    return arr;
+}
+
+function timer() {
+    var downtimer = setInterval(function () {
+        if (timeleft < 0) {
+            clearInterval(downtimer);
+            window.location.href = "../static pages/index4.html";
+        }
+        else {
+            document.getElementById("timer").innerText = "Time Left : " + timeleft;
+            timeleft -= 1;
+        }
+
+    }, 1000)
+}
+button2.addEventListener("click", function () {
+    timerCount += 1;
+
+    if (timerCount == 1) {
+        timer();
+    }
+    if (!play) {
+        play = true;
+        button2.innerHTML = "Guess";
+        guess.classList.toggle("hidden");
+        newWords2 = createNewWords2();
+        randWords2 = scrambleWords2(newWords2.split(""));
+        //console.log(randWords.join(""));
+        msg.innerHTML = randWords2;
+
+    }
+    else {
+        let tempword = guess.value;
+        if (tempword == newWords2) {
+            //console.log("Correct");
+            play = false;
+            msg.innerHTML = `AWESOME`;
+            count++;
+            score.innerText = "   Score : " + count;
+            button2.innerHTML = "NEXT";
+            guess.classList.toggle("hidden");
+            guess.value = "";
+
+
+
+        }
+        else {
+            //console.log("Incorrect");
+            msg.innerHTML = `OH NO!. Try Again: ${randWords2}`;
+        }
+    }
+})
+
+
